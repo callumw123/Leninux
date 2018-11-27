e581c2dc859c2c776a958aedc220978325d2e2cd
social-anthrax <social-anthrax@users.noreply.github.com>
2018-11-27 19:02:40 +0000

    downloader almost finished now just to make it run shit

--- Windows/downloader.py
+++ Windows/downloader.py
@@ -1,6 +1,6 @@
 import os
-
-
+import stat
+import shutil
 import shutil
 import tempfile
 
@@ -20,6 +20,11 @@ def install_and_import(package, packageName):
         globals()[package] = importlib.import_module(packageName)
 
 
+def remove_readonly(func, path, excinfo):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+
 install_and_import("shutil", "shutil")
 install_and_import("gitpython", "git")
 from git import Repo
@@ -37,17 +42,32 @@ t = tempfile.mkdtemp()
 Repo.clone_from('https://github.com/callumw123/Leninux/',
                 t, branch='master', depth=1)
 # Copy desired file from temporary dir
-shutil.move(os.path.join(t, 'windows/filestodownload.txt'), '.')
+try:
+    shutil.move(os.path.join(t, 'windows/filestodownload.txt'), '.')
+except:
+    print("file  already exists")
 
 filestodownload = open("filestodownload.txt").read()
 
 for file in filestodownload.splitlines():
-
-    shutil.move(os.path.join(t, 'windows/' + file), '.')
-
+    try:
+        shutil.move(os.path.join(t, 'windows/' + file), '.')
+    except:
+        print("file %s already exists" % file)
 
 # Remove temporary dir
-# shutil.rmtree(t)
+shutil.rmtree(t, onerror=remove_readonly)
+
+userinput = input(
+    "are you sure you want to run these files straight away? (y / n) \n").upper()
+if userinput.upper() == "Y":
+    print("files downloaded to %s" % os.getcwd())
+
+    # import subprocess
+    # subprocess.check_call([package])  # try to run downloaded files
+else:
+    print("files downloaded to %s" % os.getcwd())
+
 
 # https://stackoverflow.com/questions/51239168/how-to-download-single-file-from-a-git-repository-using-python
 # https://stackoverflow.com/questions/14120502/how-to-download-and-write-a-file-from-github-using-requests
