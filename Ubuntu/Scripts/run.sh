9f7c76d07959c95dd7f9cbdac694c732d79cb7e4
callumw123 <44530685+callumw123@users.noreply.github.com>
2019-10-13 09:34:39 -0700

    Create run.sh

---
+++ Ubuntu/Scripts/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+currentUser=$(whoami)
+
+if [[ " $currentUser " != *"root"* ]];
+then
+	#Not root
+	echo "The script must be run as root, use sudo!"
+	exit
+fi
+
+#Check Directory
+echo "Ensure you are running this script in the scripts folder!"
+read -p "Continue (Y/N): " confirmation
+
+if [[ " $confirmation " != *"Y"* ]];
+then
+	#Not confirmed
+	echo "Operation Cancelled"
+	exit
+fi
+
+gnome-terminal -x ./UpdaterScript.sh
+gnome-terminal -x ./UserScript.sh
+gnome-terminal -x ./SystemScan.sh
