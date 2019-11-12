889f10124f23e13f375152dfc51755f7923ba6fd
callumw123 <44530685+callumw123@users.noreply.github.com>
2019-11-12 10:56:52 +0000

    DNS Cache Cleared added

--- Windows/batch/host.bat
+++ Windows/batch/host.bat
@@ -5,3 +5,5 @@ echo #lorne woz here > C:\Windows\System32\drivers\etc\hosts
 attrib +r +s C:\WINDOWS\system32\drivers\etc\hosts
 echo Cleared hosts file
 echo if that didnt work then i would suggest completely emptying it
+ipconfig /flushdns
+echo DNS Cache Cleared
